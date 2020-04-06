d5723f2afb6c042b59c91d8feebf24690fcd2d62
Otis <otis@vinova.com.sg>
2020-04-06 21:29:06 +0700

    * add block

---
+++ BlockParty/ContentBlockerRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  BlockParty
+//
+//  Created by admin on 06/04/2020.
+//  Copyright © 2020 nxsang063@gmail.com. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
